d2cd4ca8a8c41f12a0638fa33ab4cecffab4d308
MileneDias <milene_diasdasilva@hotmail.com>
2021-09-09 22:41:30 -0300

    routes

---
+++ src/app/routes/aluno.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import AlunoController from '../controllers/AlunoController';
+
+const routes = new Router();
+
+routes.get('/alunos', AlunoController.index);
+routes.get('/alunos/:id', AlunoController.show);
+routes.post('alunos', AlunoController.store);
+routes.delete('/alunos/:id', AlunoController.delete);
+
+export default routes;
